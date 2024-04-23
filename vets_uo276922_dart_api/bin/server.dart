86ed072c26af0bae8086d5ab877f4669d0a3c8e4
José Rodríguez Rico <90771999+uo276922@users.noreply.github.com>
2024-04-23 22:10:03 +0200

    vets-UO276922-dart-api-02-Creación del database manager.

--- vets_uo276922_dart_api/bin/server.dart
+++ vets_uo276922_dart_api/bin/server.dart
@@ -4,6 +4,8 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'dart:convert';
+import 'package:vets_uo276922_dart_api/db_manager.dart';
+
 
 // Configure routes.
 final _router = Router()
@@ -20,14 +22,12 @@ Response _echoHandler(Request request) {
   return Response.ok('$message\n');
 }
 
-Response _usersHandler(Request request) {
-  List<Map<String, dynamic>> users = [
-    {'name': 'Juan', 'email': 'juan@example.com'},
-    {'name': 'Maria', 'email': 'maria@example.com'},
-    {'name': 'Alex', 'email': 'alez@example.com'}
-  ];
-  return Response.ok(json.encode(users));
+Future<Response> _usersHandler(Request request) async {
+DbManager dbManager = DbManager.collection("users");
+final users = await dbManager.findAll();
+return Response.ok(json.encode(users));
 }
+
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
